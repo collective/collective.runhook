62b1e1bd606851a9827fe00c153c96b3d0ff79e0
Asko Soukka <asko.soukka@iki.fi>
2014-09-25 15:43:41 +0300

    Back to development: 0.9.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='collective.runhook',
-    version='0.9.3',
+    version='0.9.4.dev0',
     description=(
         'Named instance run script entry points for plone.recipe.zope2instance'
     ),
